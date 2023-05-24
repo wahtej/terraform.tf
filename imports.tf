204056c0a740a1b05ae133d5dfe2bb80d12e9afe
wahtej <125274697+wahtej@users.noreply.github.com>
2023-05-24 15:54:58 +0530

    Delete imports.tf

--- imports.tf
+++
@@ -1,7 +0,0 @@
-# Import play books 
-- name: Install Kubernetes Dependencies
-  import_playbook: k8sdepend.yml
-- name: Initialaize Kubernetes Master
-  import_playbook: k8s_mastr.yml
-- name: Get token from master and join workers
-  import_playbook: workerscluster.yml
